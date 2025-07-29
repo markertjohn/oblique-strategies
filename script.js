34792eadd7513021891c126ed4dccdc492b267e7
markertjohn <38411035+markertjohn@users.noreply.github.com>
2025-07-29 09:54:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const strategies = [
+  "Use an old idea",
+  "Honor thy error as a hidden intention",
+  "Repetition is a form of change",
+  "When in doubt, keep it fun",
+  "Use the whole animal"
+  // Add your full list here
+];
+
+document.getElementById('draw').addEventListener('click', () => {
+  const card = strategies[Math.floor(Math.random() * strategies.length)];
+  document.getElementById('card').innerText = card;
+});
 
